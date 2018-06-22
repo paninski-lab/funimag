127a127859b616160e2d70f66c71a8f7d3fffb44
Ding Zhou <dz2336@columbia.edu>
2018-06-21 22:02:39 -0400

    update demix demo, update demix main function name

--- funimag/superpixel_analysis.py
+++ funimag/superpixel_analysis.py
@@ -1109,7 +1109,7 @@ def l1_tf(y, sigma):
     return np.asarray(x.value).flatten()
 
 
-def axon_pipeline(Yd, U, V, cut_off_point=[0.95,0.9], length_cut=[15,10], th=[2,1], pass_num=1, residual_cut = [0.6,0.6],
+def demix(Yd, U, V, cut_off_point=[0.95,0.9], length_cut=[15,10], th=[2,1], pass_num=1, residual_cut = [0.6,0.6],
                     corr_th_fix=0.31, max_allow_neuron_size=0.3, merge_corr_thr=0.6, merge_overlap_thr=0.6, num_plane=1, patch_size=[100,100],
                     plot_en=False, TF=False, fudge_factor=1, text=True, bg=False, max_iter=35, max_iter_fin=50, update_after=4):
     """
@@ -1237,7 +1237,7 @@ def axon_pipeline(Yd, U, V, cut_off_point=[0.95,0.9], length_cut=[15,10], th=[2,
         else:
             print("find superpixels!")
             connect_mat_1, idx, comps, permute_col = find_superpixel(Yt,cut_off_point[ii],length_cut[ii],eight_neighbours=True);
-        print(time.time()-start);
+        print("time: " + str(time.time()-start));
 
         start = time.time();
         print("rank 1 svd!")
@@ -1246,7 +1246,7 @@ def axon_pipeline(Yd, U, V, cut_off_point=[0.95,0.9], length_cut=[15,10], th=[2,
         else:
             c_ini, a_ini, ff, fb = spatial_temporal_ini(Yt, comps, idx, length_cut[ii], bg=bg);
             #return ff
-        print(time.time()-start);
+        print("time: " + str(time.time()-start));
         unique_pix = np.asarray(np.sort(np.unique(connect_mat_1)),dtype="int");
         unique_pix = unique_pix[np.nonzero(unique_pix)];
         #unique_pix = np.asarray(np.sort(np.unique(connect_mat_1))[1:]); #search_superpixel_in_range(connect_mat_1, permute_col, V_mat);
@@ -1269,7 +1269,7 @@ def axon_pipeline(Yd, U, V, cut_off_point=[0.95,0.9], length_cut=[15,10], th=[2,
                 pure_pix = np.hstack((pure_pix, unique_pix_temp[pure_pix_temp]));
         pure_pix = np.unique(pure_pix);
 
-        print(time.time()-start);
+        print("time: " + str(time.time()-start));
 
         start = time.time();
         print("prepare iteration!")
@@ -1279,7 +1279,7 @@ def axon_pipeline(Yd, U, V, cut_off_point=[0.95,0.9], length_cut=[15,10], th=[2,
             c = np.hstack((c, c_ini));
         else:
             a, c, b, normalize_factor, brightness_rank = prepare_iteration(Yd, connect_mat_1, permute_col, pure_pix, a_ini, c_ini, more=True);
-        print(time.time()-start);
+        print("time: " + str(time.time()-start));
 
         if plot_en:
             Cnt = local_correlations_fft(Yt);
@@ -1289,7 +1289,7 @@ def axon_pipeline(Yd, U, V, cut_off_point=[0.95,0.9], length_cut=[15,10], th=[2,
             maxiter = max_iter_fin;
         else:
             maxiter=max_iter;
-        start_time = time.time();
+        start = time.time();
         if bg:
             a, c, b, fb, ff, res, corr_img_all_r, num_list = update_AC_bg_l2(U, V, normalize_factor, a, c, b, ff, fb, dims,
                                         corr_th_fix, maxiter=maxiter, tol=1e-8, update_after=update_after,
@@ -1299,13 +1299,13 @@ def axon_pipeline(Yd, U, V, cut_off_point=[0.95,0.9], length_cut=[15,10], th=[2,
             a, c, b, fb, ff, res, corr_img_all_r, num_list = update_AC_l2(U, V, normalize_factor, a, c, b, dims,
                                         corr_th_fix, maxiter=maxiter, tol=1e-8, update_after=update_after,
                                         merge_corr_thr=merge_corr_thr,merge_overlap_thr=merge_overlap_thr, num_plane=num_plane, plot_en=plot_en, max_allow_neuron_size=max_allow_neuron_size);
-        print(time.time()-start_time);
+        print("time: " + str(time.time()-start));
         superpixel_rlt.append({'connect_mat_1':connect_mat_1, 'pure_pix':pure_pix, 'unique_pix':unique_pix, 'brightness_rank':brightness_rank, 'brightness_rank_sup':brightness_rank_sup});
         if pass_num > 1 and ii == 0:
             rlt = {'a':a, 'c':c, 'b':b, "fb":fb, "ff":ff, 'res':res, 'corr_img_all_r':corr_img_all_r, 'num_list':num_list};
             a0 = a.copy();
         ii = ii+1;
-    start_time = time.time();
+    start = time.time();
     c_tf = [];
     if TF:
         sigma = noise_estimator(c.T);
@@ -1313,7 +1313,7 @@ def axon_pipeline(Yd, U, V, cut_off_point=[0.95,0.9], length_cut=[15,10], th=[2,
         for ii in range(c.shape[1]):
             c_tf = np.hstack((c_tf, l1_tf(c[:,ii], sigma[ii])));
         c_tf = c_tf.reshape(T,int(c_tf.shape[0]/T),order="F");
-    print(time.time()-start_time);
+    print("time: " + str(time.time()-start));
 
     if plot_en:
         if pass_num > 1:
@@ -1346,8 +1346,8 @@ def axon_pipeline(Yd, U, V, cut_off_point=[0.95,0.9], length_cut=[15,10], th=[2,
 def extract_pure_and_superpixels(Yd, cut_off_point=0.95, length_cut=15, th=2, residual_cut = 0.6, num_plane=1, patch_size=[100,100], plot_en=False, text=False):
     """
     This function is only doing the superpixel initialization for one pass.
-    For parameters meanings, please refer to axon_pipeline function.
-    However, note that cut_off_point, length_cut, th and residual_cut are float numbers not lists as in axon_pipeline function, because this function could only handle one pass.
+    For parameters meanings, please refer to demix function.
+    However, note that cut_off_point, length_cut, th and residual_cut are float numbers not lists as in demix function, because this function could only handle one pass.
 
     Output:
     a_ini: 2D np.ndarray (d1xd2) x K, K is number of pure superpixels
@@ -2399,13 +2399,13 @@ def update_AC_bg_l2_Y(U, normalize_factor, a, c, b, ff, fb, patch_size, corr_th_
     return a, c, b, fb, ff, res, corr_img_all_r, num_list
 
 
-def axon_pipeline_Y(Yd, cut_off_point=[0.95,0.9], length_cut=[15,10], th=[2,1], pass_num=1, residual_cut = [0.6,0.6],
+def demix_whole_data(Yd, cut_off_point=[0.95,0.9], length_cut=[15,10], th=[2,1], pass_num=1, residual_cut = [0.6,0.6],
                     corr_th_fix=0.31, max_allow_neuron_size=0.3, merge_corr_thr=0.6, merge_overlap_thr=0.6, num_plane=1, patch_size=[100,100],
                     plot_en=False, TF=False, fudge_factor=1, text=True, bg=False, max_iter=35, max_iter_fin=50,
                     update_after=4):
     """
     This function is the demixing pipeline for whole data.
-    For parameters and output, please refer to axon_pipeline function (demixing pipeline for low rank data).
+    For parameters and output, please refer to demix function (demixing pipeline for low rank data).
     """
     ## if data has negative values then do pixel-wise minimum subtraction ##
     Yd_min = Yd.min();
@@ -2447,7 +2447,7 @@ def axon_pipeline_Y(Yd, cut_off_point=[0.95,0.9], length_cut=[15,10], th=[2,1], 
         else:
             print("find superpixels!")
             connect_mat_1, idx, comps, permute_col = find_superpixel(Yt,cut_off_point[ii],length_cut[ii],eight_neighbours=True);
-        print(time.time()-start);
+        print("time: " + str(time.time()-start));
 
         start = time.time();
         print("rank 1 svd!")
@@ -2456,7 +2456,7 @@ def axon_pipeline_Y(Yd, cut_off_point=[0.95,0.9], length_cut=[15,10], th=[2,1], 
         else:
             c_ini, a_ini, ff, fb = spatial_temporal_ini(Yt, comps, idx, length_cut[ii], bg=bg);
             #return ff
-        print(time.time()-start);
+        print("time: " + str(time.time()-start));
         unique_pix = np.asarray(np.sort(np.unique(connect_mat_1)),dtype="int");
         unique_pix = unique_pix[np.nonzero(unique_pix)];
         #unique_pix = np.asarray(np.sort(np.unique(connect_mat_1))[1:]); #search_superpixel_in_range(connect_mat_1, permute_col, V_mat);
@@ -2479,7 +2479,7 @@ def axon_pipeline_Y(Yd, cut_off_point=[0.95,0.9], length_cut=[15,10], th=[2,1], 
                 pure_pix = np.hstack((pure_pix, unique_pix_temp[pure_pix_temp]));
         pure_pix = np.unique(pure_pix);
 
-        print(time.time()-start);
+        print("time: " + str(time.time()-start));
 
         start = time.time();
         print("prepare iteration!")
@@ -2490,7 +2490,7 @@ def axon_pipeline_Y(Yd, cut_off_point=[0.95,0.9], length_cut=[15,10], th=[2,1], 
         else:
             a, c, b, normalize_factor, brightness_rank = prepare_iteration(Yd, connect_mat_1, permute_col, pure_pix, a_ini, c_ini, more=True);
 
-        print(time.time()-start);
+        print("time: " + str(time.time()-start));
 
         if plot_en:
             Cnt = local_correlations_fft(Yt);
@@ -2500,7 +2500,7 @@ def axon_pipeline_Y(Yd, cut_off_point=[0.95,0.9], length_cut=[15,10], th=[2,1], 
             maxiter = max_iter_fin;
         else:
             maxiter=max_iter;
-        start_time = time.time();
+        start = time.time();
         if bg:
             a, c, b, fb, ff, res, corr_img_all_r, num_list = update_AC_bg_l2_Y(Yd.reshape(np.prod(dims),-1,order="F"), normalize_factor, a, c, b, ff, fb, dims,
                                         corr_th_fix, maxiter=maxiter, tol=1e-8, update_after=update_after,
@@ -2510,7 +2510,7 @@ def axon_pipeline_Y(Yd, cut_off_point=[0.95,0.9], length_cut=[15,10], th=[2,1], 
             a, c, b, fb, ff, res, corr_img_all_r, num_list = update_AC_l2_Y(Yd.reshape(np.prod(dims),-1,order="F"), normalize_factor, a, c, b, dims,
                                         corr_th_fix, maxiter=maxiter, tol=1e-8, update_after=update_after,
                                         merge_corr_thr=merge_corr_thr,merge_overlap_thr=merge_overlap_thr, num_plane=num_plane, plot_en=plot_en, max_allow_neuron_size=max_allow_neuron_size);
-        print(time.time()-start_time);
+        print("time: " + str(time.time()-start));
         superpixel_rlt.append({'connect_mat_1':connect_mat_1, 'pure_pix':pure_pix, 'unique_pix':unique_pix, 'brightness_rank':brightness_rank, 'brightness_rank_sup':brightness_rank_sup});
         if pass_num > 1 and ii == 0:
             rlt = {'a':a, 'c':c, 'b':b, "fb":fb, "ff":ff, 'res':res, 'corr_img_all_r':corr_img_all_r, 'num_list':num_list};
@@ -2518,12 +2518,14 @@ def axon_pipeline_Y(Yd, cut_off_point=[0.95,0.9], length_cut=[15,10], th=[2,1], 
         ii = ii+1;
 
     c_tf = [];
+    start = time.time();
     if TF:
         sigma = noise_estimator(c.T);
         sigma *= fudge_factor
         for ii in range(c.shape[1]):
             c_tf = np.hstack((c_tf, l1_tf(c[:,ii], sigma[ii])));
         c_tf = c_tf.reshape(T,int(c_tf.shape[0]/T),order="F");
+    print("time: " + str(time.time()-start));
     if plot_en:
         if pass_num > 1:
             spatial_sum_plot(a0, a, dims, num_list, text);
